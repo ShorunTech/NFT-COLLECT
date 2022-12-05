267048c5e1ec2bee807387114ec8cdc70ac7c2c0
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-05 15:01:12 +0100

    cryptodev

--- hardhat-tutor/contracts/CryptoDevs.sol
+++ hardhat-tutor/contracts/CryptoDevs.sol
@@ -4,3 +4,11 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+    /**
+      * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+      * token will be the concatenation of the `baseURI` and the `tokenId`.
+      */
+    string _baseTokenURI;
+    
