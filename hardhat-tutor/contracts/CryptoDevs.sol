b263694f52c9078f2a145380ab3daf4b2f85fb82
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-05 15:02:33 +0100

    cryptodevs

--- hardhat-tutor/contracts/CryptoDevs.sol
+++ hardhat-tutor/contracts/CryptoDevs.sol
@@ -12,3 +12,8 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
       */
     string _baseTokenURI;
     
+     //  _price is the price of one Crypto Dev NFT
+    uint256 public _price = 0.01 ether;
+
+    // _paused is used to pause the contract in case of an emergency
+    bool public _paused;
