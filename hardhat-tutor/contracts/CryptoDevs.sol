4811b489f73dd5798700199dd47740121dc666a0
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-06 09:26:07 +0100

    CryptoDevs

--- hardhat-tutor/contracts/CryptoDevs.sol
+++ hardhat-tutor/contracts/CryptoDevs.sol
@@ -49,5 +49,16 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
         whitelist = IWhitelist(whitelistContract);
     }
 
+     /**
+      * @dev startPresale starts a presale for the whitelisted addresses
+      */
+    function startPresale() public onlyOwner {
+        presaleStarted = true;
+        // Set presaleEnded time as current timestamp + 5 minutes
+        // Solidity has cool syntax for timestamps (seconds, minutes, hours, days, years)
+        presaleEnded = block.timestamp + 5 minutes;
+    }
+
+
 
       
